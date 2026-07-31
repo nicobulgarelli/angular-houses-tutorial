27c1a78345fb06a95f51e114c9e08d9ac5f4715c
nicobulga <nicobulgarelli@users.noreply.github.com>
2026-07-31 17:16:25 +0200

    rimossi commenti

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,22 +1,21 @@
 import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import {HousingLocation} from '../housing-location/housing-location';
 import {HousingLocationInfo} from '../housinglocation';
-import {Housing} from '../housing';
+import {Housing} from '../housing';git status
 
-@Component({              //componente quindi elemento mostrato sulla pagina
-  selector: 'app-home',     //il componente è richiamato dal nome app-home
+@Component({
+  selector: 'app-home',
   imports: [HousingLocation],
-  //template = come viene rappresentata la pagina in html
   template: `       
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" #filter />     <!--#filter = etichetta applicata a input, per chiamarlo al bisogno-->
-        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>   <!--(click) quando è cliccato avvia quella funzione-->
+        <input type="text" placeholder="Filter by city" #filter />     
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>   
       </form>
     </section>
     <section class="results">
-    @for (housingLocation of filteredLocationList; track $index) {  <!--repeatedly renders content of a block for each item in a collection. track $index(????)-->
-      <app-housing-location [housingLocation]="housingLocation" />   <!--Index tiene traccia delle posizioni ed è il numero della posizione corrente, serve per capire quali case sono le stesse quando ridisegna la lista-->
+    @for (housingLocation of filteredLocationList; track $index) { 
+      <app-housing-location [housingLocation]="housingLocation" />   
     }
     </section>
   `,
